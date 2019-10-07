929a7ebe5f1f409ea8c4ecc7e9270408541d9bf9
binnyFriedman <binny@nekuda.co.il>
2019-10-07 15:13:41 +0300

    dvd

--- index.js
+++ index.js
@@ -40,7 +40,6 @@ app.use(morgan("dev"));
 app.use("/users", users);
 app.use("/quotes", quotes);
 app.use("/services", services);
-// app.use(connect.static("public"));
 app.listen(3000, "0.0.0.0", () => console.log("listening"));
 
 module.exports = app;
